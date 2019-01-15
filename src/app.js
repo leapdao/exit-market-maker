a2f184d226eb27c96863c92ec42462499cafb5bd
Jan <jan@parseclabs.org>
2019-01-15 18:07:04 +0100

    Prototype

---
+++ src/app.js
@@ -0,0 +1,104 @@
+
+const axios = require('axios');
+const leap = require('leap-core');
+const Web3 = require('web3');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const abi = require('./abi');
+
+const provider = new HDWalletProvider(process.env.PRIV_KEY, process.env.ETH_NETWORK);
+const web3 = new Web3(provider);
+const exitHandler = new web3.eth.Contract(abi, process.env.EXIT_HANDLER);
+
+let response;
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ * @param {string} event.resource - Resource path.
+ * @param {string} event.path - Path parameter.
+ * @param {string} event.httpMethod - Incoming request's method name.
+ * @param {Object} event.headers - Incoming request headers.
+ * @param {Object} event.queryStringParameters - query string parameters.
+ * @param {Object} event.pathParameters - path parameters.
+ * @param {Object} event.stageVariables - Applicable stage variables.
+ * @param {Object} event.requestContext - Request context, including authorizer-returned key-value pairs, requestId, sourceIp, etc.
+ * @param {Object} event.body - A JSON string of the request payload.
+ * @param {boolean} event.body.isBase64Encoded - A boolean flag to indicate if the applicable request payload is Base64-encode
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ * @param {string} context.logGroupName - Cloudwatch Log Group name
+ * @param {string} context.logStreamName - Cloudwatch Log stream name.
+ * @param {string} context.functionName - Lambda function name.
+ * @param {string} context.memoryLimitInMB - Function memory.
+ * @param {string} context.functionVersion - Function version identifier.
+ * @param {function} context.getRemainingTimeInMillis - Time in milliseconds before function times out.
+ * @param {string} context.awsRequestId - Lambda request ID.
+ * @param {string} context.invokedFunctionArn - Function ARN.
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * @returns {boolean} object.isBase64Encoded - A boolean flag to indicate if the applicable payload is Base64-encode (binary support)
+ * @returns {string} object.statusCode - HTTP Status Code to be returned to the client
+ * @returns {Object} object.headers - HTTP Headers to be returned
+ * @returns {Object} object.body - JSON Payload to be returned
+ * 
+ */
+exports.lambdaHandler = async (event, context) => {
+    const from = (await web3.eth.getAccounts())[0];
+    try {
+        const data = JSON.parse(event.body);
+        console.log(data);
+        const tx = leap.Exit.txFromProof(data.transferProof);
+        const sellValue = parseInt(data.signedData[1]);
+        const utxoValue = tx.outputs[data.outputIndex].value;
+
+        if (sellValue < process.env.RATE*utxoValue) {
+            response = {
+                'statusCode': 400,
+                'body': JSON.stringify({
+                    message : "Price too low"
+                })
+            }
+        } else {
+
+            const sendTx = () => {
+                return new Promise((resolve, reject) => {
+                    exitHandler.methods.startBoughtExit(
+                        data.inputProof, 
+                        data.transferProof,
+                        data.outputIndex,
+                        data.inputIndex,
+                        data.signedData)
+                    .send({
+                        from: from,
+                        gasPrice: process.env.GAS_PRICE,
+                        gas: 2000000
+                    }, (error, transactionHash) => {
+                        if (error) {
+                            reject(error);
+                        } else {
+                            resolve(transactionHash);
+                        }
+                    })
+                });
+            }
+
+            const txHash = await sendTx();
+            console.log(txHash);
+
+            response = {
+                'statusCode': 200,
+                'body': JSON.stringify({
+                    txHash: txHash
+                })
+            }
+        }        
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response;
+};
---
+++ src/tests/api/test-api.js
@@ -0,0 +1,45 @@
+const leap = require('leap-core');
+const axios = require('axios');
+
+const url = "http://localhost:3000/exit";
+const alice = "0x83B3525e17F9eAA92dAE3f9924cc333c94C7E98a";
+const alicePriv = "0xbd54b17c48ac1fc91d5ef2ef02e9911337f8758e93c801b619e5d178094486cc";
+const exitHandler = "0x791186143a8fe5f0287f0DC35df3A71354f607b6";
+
+async function test() {
+
+  const deposit = leap.Tx.deposit(0, 100, alice);
+  let transfer = leap.Tx.transfer(
+    [new leap.Input(new leap.Outpoint(deposit.hash(), 0))],
+    [new leap.Output(50, exitHandler), new leap.Output(50, alice)]
+  );
+  transfer = transfer.sign([alicePriv]);
+
+  const block = new leap.Block(33);
+  block.addTx(deposit).addTx(transfer);
+
+  const prevPeriodRoot = "0x32C220482C68413FBF8290E3B1E49B0A85901CFCD62AB0738760568A2A6E8A57";
+  const period = new leap.Period(prevPeriodRoot, [block]);
+
+  const transferProof = period.proof(transfer);
+  const depositProof = period.proof(deposit);
+
+  const outputIndex = 0;
+  const inputIndex = 0;
+
+  const sellPrice = 49;
+  const utxoId = (new leap.Outpoint(transfer.hash(), 0)).getUtxoId();
+
+  const signedData = leap.Exit.signOverExit(utxoId, sellPrice, alicePriv);
+  const signedDataBytes32 = leap.Exit.bufferToBytes32Array(signedData);
+
+  let response = await axios.post(url, {
+    inputProof: depositProof,
+    transferProof: transferProof,
+    inputIndex: inputIndex,
+    outputIndex: outputIndex,
+    signedData: signedDataBytes32
+  });
+  console.log(response.data);
+};
+test();
